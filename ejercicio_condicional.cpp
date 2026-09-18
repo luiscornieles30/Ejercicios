b5b5f832305019fdb85dbdb690b125994bafbdfb
Luis <luiscornieles30@gmail.com>
2026-09-18 16:41:28 -0400

    Solucion de bugs 1/1

--- ejercicio_condicional.cpp
+++ ejercicio_condicional.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+// El siguente ejercicio contiene una estructura condicional.
 int main() {
- 
+  int minutos= 0;
+  int precioxminuto= 0;
+  float IVA= 0; // Declaramos las variables que usaremos, y aprovechamos de establecerlas en 0 para evitar basuras de memoria.
+  std::cout << "Ingrese la cantidad de minutos que desea usar: ";
+  std::cin >> minutos; // Pedimos al usuario que ingrese la cantidad de minutos que desea usar.
+  if (std::cin.fail()) {
+    std::cout << "Error: Por favor, ingrese un valor valido." << std::endl;
+  }
 
     return 0;
 }
