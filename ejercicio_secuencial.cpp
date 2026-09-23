e486f88060f67c843d438723c66896db52109d52
Luis Cornieles <97906798+luiscornieles30@users.noreply.github.com>
2026-09-23 11:29:48 -0400

    STD

--- ejercicio_secuencial.cpp
+++ ejercicio_secuencial.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <ctime>
 #include <cstdlib>
+using namespace std;
 // El siguente ejercicio contiene una estructura secuencial.
 int main()
  {
@@ -10,14 +11,14 @@ int main()
     srand(time(0)); // Generamos la semilla con la que trabajará nuestra libreria y pueda dar numeros aleatorios con el tiempo de ejecución real del programa.
     
 
-    std::cout << "Ingrese el modelo de carro que desea comprar: " << std::endl;
-    std::cin >> modelo;
+    cout << "Ingrese el modelo de carro que desea comprar: " << endl;
+    cin >> modelo;
     carro = 100 + rand() % (2000 - 100 + 1); // Generamos un rango de precio aleatorio para que cada ves que se ejecute el programa el precio del carro sea diferente.
     impuesto = carro * 0.06; // Calculamos el impuesto.
     ganancia = carro * 0.12; // Calculamos la ganancia.
     precioreal = (impuesto + ganancia);
     precioreal = carro + precioreal; // Calculamos el precio real del carro.
-    std::cout << "El precio del carro modelo " << modelo << " es: " << carro << "bs" << std::endl;
-    std::cout << "El coste final del vehículo " << modelo << " es: " << precioreal << "bs" << std::endl;
+    cout << "El precio del carro modelo " << modelo << " es: " << carro << "bs" << endl;
+    cout << "El coste final del vehículo " << modelo << " es: " << precioreal << "bs" << endl;
     return 0;
 }
